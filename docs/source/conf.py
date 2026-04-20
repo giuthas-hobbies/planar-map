a5e15c979cfd94d0848aa0804b203b9992fbd3cb
Pertti Palo <giuthas@gmail.com>
2026-04-19 22:28:12 -0600

    Update doc theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 
 # Uncomment this AND deal with the resulting errors before trying to run doc
